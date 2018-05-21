9ca43cc9d5400ddd6a85e53c58662030b9828525
joyce <joycedelatorre87@gmail.com>
2018-05-21 11:09:00 -0500

    rewrite of schema

--- schema.sql
+++ schema.sql
@@ -1,9 +1,30 @@
+DROP DATABASE IF EXISTS chirpy;
 CREATE DATABASE chirpy;
 USE chirpy;
 
-`id` Int(11) AUTO_INCREMENT  NOT NULL,
+
+CREATE TABLE message(
+
+`id` Int(11) AUTO_INCREMENT  NOT NULL,    
 `author` VARCHAR (255) NOT NULL,
 `body` VARCHAR (255) NOT NULL,
 `created_at` DATETIME NOT NULL,
 
 PRIMARY KEY (`id`);
+);
+
+-- Note the backticks dont work on sql pro
+
+
+------------------
+CREATE DATABASE chirpy;
+USE chirpy;
+
+CREATE TABLE message(
+id INT(11) AUTO_INCREMENT NOT NULL,
+author VARCHAR (255) NOT NULL,
+body VARCHAR (255) NOT NULL,
+created_at DATETIME NOT NULL,
+
+PRIMARY KEY (id)
+);
